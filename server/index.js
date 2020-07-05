f24d6402afe529525404b26b71c93d0b7f904a2c
tfjkvar <tfjkvar@gmail.com>
2020-07-05 22:30:27 +0900

    add module

--- server/index.js
+++ server/index.js
@@ -1,36 +1,36 @@
-// const compression = require('compression');
-// const path = require('path');
-// const express = require('express');
-// const app = express();
-// const port = process.env.PORT || 8080;
-
-// // Gzip
-// app.use(compression());
-
-// // Run the app by serving the static files
-// // in the dist directory
-// app.use(express.static(__dirname + '/../dist'));
-
-// // Start the app by listening on the default
-// // Heroku port
-// app.listen(port);
-
-// // For all GET requests, send back index.html
-// // so that PathLocationStrategy can be used
-// app.get('/*', function(req, res) {
-//   res.sendFile(path.join(__dirname + '/../dist/index.html'));
-// });
-
-// console.log(`Server listening on ${port}`);
-
-// Install express server 
-const express = require('express');
+const compression = require('compression');
 const path = require('path');
+const express = require('express');
 const app = express();
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/website'));
+const port = process.env.PORT || 8080;
+
+// Gzip
+app.use(compression());
+
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/../dist'));
+
+// Start the app by listening on the default
+// Heroku port
+app.listen(port);
+
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/website/index.html'));
+  res.sendFile(path.join(__dirname + '/../dist/index.html'));
 });
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+
+console.log(`Server listening on ${port}`);
+
+// // Install express server 
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+// // Serve only the static files form the dist directory
+// app.use(express.static(__dirname + '/dist/website'));
+// app.get('/*', function(req, res) {
+//   res.sendFile(path.join(__dirname + '/dist/website/index.html'));
+// });
+// // Start the app by listening on the default Heroku port
+// app.listen(process.env.PORT || 8080);
