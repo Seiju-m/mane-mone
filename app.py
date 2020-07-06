60c0ebb1b22485f0106aed81b3a8b4aba3abd681
tfjkvar <tfjkvar@gmail.com>
2020-07-06 23:15:34 +0900

    modify main page

--- app.py
+++ app.py
@@ -57,7 +57,8 @@ def login():
         if(request.form["username"] in user_check and request.form["password"] == user_check[request.form["username"]]["password"]):
             # ユーザーが存在した場合はログイン
             login_user(users.get(user_check[request.form["username"]]["id"]))
-            title = 'aaa'
+            title = 0
+            # data = User.query.filter_by(user='tfjkv').first()
             return render_template("mana-mone.html",  title=title)
         else:
             return abort(401)
