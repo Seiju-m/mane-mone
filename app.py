74b2ba904f743157baeb3ca3fedcfd293f405a79
tfjkvar <tfjkvar@gmail.com>
2020-07-06 12:06:39 +0900

    add login

--- app.py
+++ app.py
@@ -1,22 +1,69 @@
 import os
-import io
-import time
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory, session
+from flask import Flask, request, Response, abort, render_template
+from flask_login import LoginManager, login_user, logout_user, login_required, UserMixin
+from collections import defaultdict
+
 app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.init_app(app)
+app.config['SECRET_KEY'] = "secret"
 
-# <--プログラム-->
+class User(UserMixin):
+    def __init__(self, id, name, password):
+        self.id = id
+        self.name = name
+        self.password = password
 
-@app.route('/')
-def index():
-    return 'Hello World!'
+# ログイン用ユーザー作成
+users = {
+    1: User(1, "user01", "password"),
+    2: User(2, "user02", "password")
+}
 
-@app.route('/test')
-def index2():
-    return 'Hello test!'
+# ユーザーチェックに使用する辞書作成
+nested_dict = lambda: defaultdict(nested_dict)
+user_check = nested_dict()
+for i in users.values():
+    user_check[i.name]["password"] = i.password
+    user_check[i.name]["id"] = i.id
 
-@app.route("/index") #アプリケーション/indexにアクセスが合った場合
-def index3():
-   return render_template('index.html')
+@login_manager.user_loader
+def load_user(user_id):
+    return users.get(int(user_id))
+
+# @app.route('/')
+# def home():
+#     return Response("home: <a href='/login/'>Login</a> <a href='/protected/'>Protected</a> <a href='/logout/'>Logout</a>")
+
+# ログインしないと表示されないパス
+@app.route('/protected/')
+@login_required
+def protected():
+    return render_template("dashboard.html")
+
+# ログインパス
+@app.route('/', methods=["GET", "POST"])
+def login():
+    if(request.method == "POST"):
+        # ユーザーチェック
+        if(request.form["username"] in user_check and request.form["password"] == user_check[request.form["username"]]["password"]):
+            # ユーザーが存在した場合はログイン
+            login_user(users.get(user_check[request.form["username"]]["id"]))
+            return render_template("dashboard.html")
+        else:
+            return abort(401)
+    else:
+        return render_template("login.html")
+
+# ログアウトパス
+@app.route('/logout/')
+@login_required
+def logout():
+    logout_user()
+    return Response('''
+    logout success!<br />
+    <a href="/">login</a>
+    ''')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
