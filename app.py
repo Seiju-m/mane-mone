e08c9993c37fe63d7e3dfe06632a6e2460b3ae42
tfjkvar <tfjkvar@gmail.com>
2020-07-06 11:34:48 +0900

    add html

--- app.py
+++ app.py
@@ -14,5 +14,9 @@ def index():
 def index2():
     return 'Hello test!'
 
+@app.route("/index") #アプリケーション/indexにアクセスが合った場合
+def index():
+   return render_template('index.html')
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
