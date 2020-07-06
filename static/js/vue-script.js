92781613fc6138308cd4c636bc8147bef6b037ca
tfjkvar <tfjkvar@gmail.com>
2020-07-07 01:50:48 +0900

    modify main page

--- static/js/vue-script.js
+++ static/js/vue-script.js
@@ -1,8 +1,26 @@
 var app = new Vue({
     delimiters: ['[[', ']]'],
-    el: '#test',
-    data: {
-        hello: 'world',
+    el:'#app',
+    data:{
+       count: 0,
+       hello: 'world',
     },
-    methods: {},
-})
+    methods:{
+     plus:function(){
+      this.count++
+     }
+    }
+ })
+
+ var app2 = new Vue({
+    delimiters: ['[[', ']]'],
+    el:'#edits',
+    data:{
+    },
+    methods:{
+    edit:function(){
+      console.log("test")
+      app.count++
+     }
+    }
+ })
