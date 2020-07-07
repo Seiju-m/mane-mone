e9fd22655a07aaa34e33919e44ccbd11aae9f4bf
tfjkvar <tfjkvar@gmail.com>
2020-07-08 01:19:44 +0900

    add db ope

--- lib/calc.py
+++ lib/calc.py
@@ -1,6 +1,6 @@
 def calc(data):
     data.last_ex = data.income - data.food_ex - data.daily_ex- data.hobby_ex- data.rent_cost - data.scholar - data.utility_cost - data.other
-    data.food_ex = data.food_st - data.food_ex
+    data.food_ex2 = data.food_st - data.food_ex
     data.daily_ex = data.daily_st - data.daily_ex
     data.hobby_ex = data.hobby_st - data.hobby_ex
     return data
