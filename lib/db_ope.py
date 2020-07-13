b0596de6ab7a52cd2b336c445af16bf0def774ff
tfjkvar <tfjkvar@gmail.com>
2020-07-13 23:38:09 +0900

    change id

--- lib/db_ope.py
+++ lib/db_ope.py
@@ -4,12 +4,19 @@ from sqlalchemy.ext.declarative import declarative_base
 import os
 import re
 
+# DATABASE = 'postgresql'
+# USER = 'rsownwafjdissl'
+# PASSWORD = '04bbfe55fd79187e75d3256230844b9f0e4a2e396a40db8977e4e1e05760c177'
+# HOST = 'ec2-34-230-149-169.compute-1.amazonaws.com'
+# PORT = '5432'
+# DB_NAME = 'dar8qtfcfi65mk'
+
 DATABASE = 'postgresql'
-USER = 'rsownwafjdissl'
-PASSWORD = '04bbfe55fd79187e75d3256230844b9f0e4a2e396a40db8977e4e1e05760c177'
-HOST = 'ec2-34-230-149-169.compute-1.amazonaws.com'
+USER = 'postgres'
+PASSWORD = 'Seiju7479'
+HOST = '127.0.0.1'
 PORT = '5432'
-DB_NAME = 'dar8qtfcfi65mk'
+DB_NAME = 'manage3'
 
 CONNECT_STR = '{}://{}:{}@{}:{}/{}'.format(DATABASE, USER, PASSWORD, HOST, PORT, DB_NAME)
 
