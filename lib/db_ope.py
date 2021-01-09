3daf425687c0de99d2e6bf599a2da74c54664319
森下星需 <morishitaseiju@tfjkvarnoMacBook-Air.local>
2021-01-09 21:02:28 +0900

    add card touch

--- lib/db_ope.py
+++ lib/db_ope.py
@@ -7,20 +7,20 @@ import datetime
 import pytz
 
 ## for prod
-DATABASE = 'postgresql'
-USER = 'rsownwafjdissl'
-PASSWORD = '04bbfe55fd79187e75d3256230844b9f0e4a2e396a40db8977e4e1e05760c177'
-HOST = 'ec2-34-230-149-169.compute-1.amazonaws.com'
-PORT = '5432'
-DB_NAME = 'dar8qtfcfi65mk'
+# DATABASE = 'postgresql'
+# USER = 'rsownwafjdissl'
+# PASSWORD = '04bbfe55fd79187e75d3256230844b9f0e4a2e396a40db8977e4e1e05760c177'
+# HOST = 'ec2-34-230-149-169.compute-1.amazonaws.com'
+# PORT = '5432'
+# DB_NAME = 'dar8qtfcfi65mk'
 
 ## for dev
-# DATABASE = 'postgresql'
-# USER = 'postgres'
-# PASSWORD = 'Seiju7479'
-# HOST = '127.0.0.1'
-# PORT = '5432' 
-# DB_NAME = 'manage3'
+DATABASE = 'postgresql'
+USER = 'postgres'
+PASSWORD = 'Seiju7479'
+HOST = '127.0.0.1'
+PORT = '5432' 
+DB_NAME = 'manage3'
 
 ## connect info
 CONNECT_STR = '{}://{}:{}@{}:{}/{}'.format(DATABASE, USER, PASSWORD, HOST, PORT, DB_NAME)
